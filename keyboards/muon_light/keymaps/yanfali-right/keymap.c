237ec0fb17c6e044a8d8f67d62d273d0af63384d
Yan-Fa Li <yanfali@gmail.com>
2018-10-09 00:13:55 -0700

    update keymap for personal preferences

--- keyboards/muon_light/keymaps/yanfali-right/keymap.c
+++ keyboards/muon_light/keymaps/yanfali-right/keymap.c
@@ -56,15 +56,15 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 [_QWERTY] = LAYOUT_ortho_4x6(
   #ifdef MUON_LEFT
-    KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,
-    KC_ESC,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,
-    KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,
-    BACKLIT, KC_LCTL, KC_LALT, KC_LGUI, LOWER,   KC_SPC
+    KC_TAB,                KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,
+    MT(MOD_LCTL, KC_ESC),  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,
+    KC_LSFT,               KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,
+    BACKLIT,               KC_LCTL, KC_LALT, KC_LGUI, LOWER,   KC_SPC
   #else
-    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC,
-    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
-    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_ENT,
-    KC_SPC,  RAISE,   KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT
+    KC_Y,     KC_U,    KC_I,    KC_O,    KC_P,    KC_MINS,
+    KC_H,     KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
+    KC_N,     KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_ENT,
+    KC_BSPC,  RAISE,   KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT
   #endif
 ),
 
