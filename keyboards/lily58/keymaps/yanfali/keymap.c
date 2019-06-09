34a795826bfe2b528748adcdff72ae3dc336c364
Yan-Fa Li <yanfali@gmail.com>
2019-06-08 19:19:01 -0700

    Parens

--- keyboards/lily58/keymaps/yanfali/keymap.c
+++ keyboards/lily58/keymaps/yanfali/keymap.c
@@ -95,7 +95,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   _______, _______,  _______, _______,  _______, _______,                     _______, _______, _______, _______, _______, _______,
   KC_1,    KC_2,     KC_UP,   KC_3,     KC_4,    KC_5,                        KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    _______,
   KC_F1,   KC_LEFT,  KC_DOWN, KC_RIGHT, KC_F5,   KC_F6,                       XXXXXXX, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, XXXXXXX,
-  KC_F7,   KC_F8,    KC_F9,   KC_F10,   KC_F11,  KC_F12,   _______, _______,  KC_PLUS, KC_MINS, KC_EQL,  KC_LBRC, KC_RBRC, KC_BSLS,
+  KC_F7,   KC_F8,    KC_F9,   KC_F10,   KC_F11,  KC_F12,   KC_LPRN, KC_RPRN,  KC_PLUS, KC_MINS, KC_EQL,  KC_LBRC, KC_RBRC, KC_BSLS,
                               _______,  _______, _______,  _______, _______,  _______, _______, _______ \
 ),
 /* ADJUST
