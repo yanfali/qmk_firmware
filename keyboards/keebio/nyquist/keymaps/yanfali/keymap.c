10ea50a37526bd544102ba242cbd4c7c520a8404
Yan-Fa Li <yanfa.li@fireeye.com>
2019-08-30 09:29:12 -0700

    Tweaks
     - retro tapping and tapping term
     - move raise back to bspc it interferes with hitting space

--- keyboards/keebio/nyquist/keymaps/yanfali/keymap.c
+++ keyboards/keebio/nyquist/keymaps/yanfali/keymap.c
@@ -42,7 +42,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_TAB,                KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_EQUAL, \
   MT(MOD_LCTL, KC_ESC),  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, \
   KC_LSFT,               KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_ENT , \
-  ADJUST,                KC_LALT, KC_LGUI, LOWER,   LT(_RAISE, KC_SPC),  LT(_RAISE, KC_SPC),  KC_BSPC,  KC_NO, KC_LEFT, KC_DOWN, KC_UP, KC_RIGHT  \
+  ADJUST,                KC_LALT, KC_LGUI, LOWER,   KC_SPC,  KC_SPC,  LT(_RAISE, KC_BSPC),  KC_NO, KC_LEFT, KC_DOWN, KC_UP, KC_RIGHT  \
 ),
 
 /* Lower
