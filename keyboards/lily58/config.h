85e076778ec51d851e93cf09d9d7aff447fc1469
Yan-Fa Li <yanfa.li@fireeye.com>
2019-09-23 11:27:54 -0700

    convert everything over to use OLED

--- keyboards/lily58/config.h
+++ keyboards/lily58/config.h
@@ -25,9 +25,6 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define SERIAL_USE_MULTI_TRANSACTION
 #endif
 
-#define NO_ACTION_MACRO
-#define NO_ACTION_FUNCTION
-
 // Use the lily version to get the Lily58 logo instead of the qmk logo
 #define OLED_FONT_H "lib/glcdfont_lily.c"
 
