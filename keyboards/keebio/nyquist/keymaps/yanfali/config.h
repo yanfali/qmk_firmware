98b6c8b02f066468b00ce61b6c9f2c060d4344e0
Yan-Fa Li <yanfa.li@fireeye.com>
2019-10-03 17:40:13 -0700

    ARM branch

--- keyboards/keebio/nyquist/keymaps/yanfali/config.h
+++ keyboards/keebio/nyquist/keymaps/yanfali/config.h
@@ -22,8 +22,10 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 /* Use I2C or Serial, not both */
 
-#define USE_SERIAL
-// #define USE_I2C
+
+
+//#define USE_SERIAL
+#define USE_I2C
 
 // #define TAPPING_FORCE_HOLD
 #ifdef TAPPING_TERM
