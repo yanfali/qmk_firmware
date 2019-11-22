1d216820a4b7570ea7800ec0a8e0b16a3e7ecce4
Yan-Fa Li <yanfa.li@fireeye.com>
2019-11-21 23:58:32 -0800

    Yuhri's keymap

--- keyboards/clueboard/66/keymaps/yuhri/keymap.c
+++ keyboards/clueboard/66/keymaps/yuhri/keymap.c
@@ -1,17 +1,19 @@
-#include "66.h"
+#include "rev3.h"
 
 // Each layer gets a name for readability, which is then used in the keymap matrix below.
 // The underscores don't mean anything - you can have a layer called STUFF or any other name.
 // Layer names don't all need to be of the same length, obviously, and you can also skip them
 // entirely and just use numbers.
-#define _BL 0
-#define _FL 1
-#define _CL 2
+enum _layers {
+    _BL,
+    _FL,
+    _CL
+};
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   /* Keymap _BL: Base Layer (Default Layer)
    */
-[_BL] = LAYOUT(
+[_BL] = LAYOUT_all(
   KC_GESC,  KC_1,    KC_2,   KC_3,   KC_4,   KC_5,   KC_6,   KC_7,   KC_8,   KC_9,    KC_0,     KC_MINS,  KC_EQL,   KC_BSPC,   KC_BSPC,          KC_PGUP,
   KC_TAB,  KC_Q,    KC_W,   KC_E,   KC_R,   KC_T,   KC_Y,   KC_U,   KC_I,   KC_O,    KC_P,     KC_LBRC,  KC_RBRC,  KC_BSLS,                   KC_PGDN,
   KC_CAPS, KC_A,    KC_S,   KC_D,   KC_F,   KC_G,   KC_H,   KC_J,   KC_K,   KC_L,    KC_SCLN,  KC_QUOT,  KC_NUHS,  KC_ENT,
@@ -20,7 +22,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   /* Keymap _FL: Function Layer
    */
-[_FL] = LAYOUT(
+[_FL] = LAYOUT_all(
   KC_GRV,  KC_F1,   KC_F2,  KC_F3,  KC_F4,  KC_F5,  KC_F6,  KC_F7,  KC_F8,  KC_F9,   KC_F10,   KC_F11,   KC_F12,   _______, KC_DEL,           BL_STEP,
   _______, _______, _______,_______,_______,_______,_______,_______,_______,KC_SLCK, KC_PAUS,  _______,  _______,  _______,                   _______,
   _______, _______, MO(_CL),_______,_______,_______,_______,_______,_______,_______, _______,  _______,  _______,  _______,
@@ -29,7 +31,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   /* Keymap _CL: Reset/Underlight layer
    */
-[_CL] = LAYOUT(
+[_CL] = LAYOUT_all(
   _______, _______, _______,_______,_______,_______,_______,_______,_______,_______, _______,  _______,  _______,  _______, RGB_TOG,             RGB_VAI,
   _______, _______, _______,_______,RESET,  _______,_______,_______,_______,_______, _______,  _______,  _______,  _______,                      RGB_VAD,
   _______, _______, _______,_______,_______,_______,_______,_______,_______,_______, _______,  _______,  _______,  _______,
