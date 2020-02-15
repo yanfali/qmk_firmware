112df3e6ed20c22596f6e4657d685b2a56c18723
Yan-Fa Li <yanfa.li@fireeye.com>
2020-02-14 19:10:32 -0800

    Update keymap for doro67
     - move around RESET,
     - add EEP_RST
     - add KC_MUTE, KC_VOLU, KC_VOLD

--- keyboards/doro67/multi/keymaps/yanfali/keymap.c
+++ keyboards/doro67/multi/keymaps/yanfali/keymap.c
@@ -27,14 +27,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_TAB,           KC_Q,    KC_W,    KC_E,    KC_R,             KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS,          KC_PGUP,
     LCTL_T(KC_ESC),   KC_A,    KC_S,    KC_D,    KC_F,             KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, KC_ENT,                    KC_PGDN,
     KC_LSFT,  KC_NO,  KC_Z,    KC_X,    KC_C,    KC_V,             KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,                   KC_UP,   MO(_FN),
-    KC_LCTL,          KC_LALT, KC_LGUI, KC_SPC,  LT(_FN, KC_SPC),  KC_BSPC, KC_RGUI, KC_RALT, MO(_FN),                                     KC_LEFT, KC_DOWN, KC_RGHT
+    KC_LCTL,          KC_LALT, KC_LGUI, KC_SPC,  LT(_FN, KC_SPC),  KC_BSPC, KC_RGUI, KC_RALT, KC_NO,                                       KC_LEFT, KC_DOWN, KC_RGHT
   ),
   [_FN] = LAYOUT_multi(
-    _______, KC_F1,   KC_F2,    KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  RESET,   _______, _______,
-    BL_TOGG, BL_STEP, KC_MS_U, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
-    _______, KC_MS_L, KC_MS_D, KC_MS_R, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
-    _______, KC_BTN1, KC_BTN3, KC_BTN2, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_APP,
-    _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______
+    _______,          KC_F1,   KC_F2,    KC_F3,   KC_F4,           KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  EEP_RST, _______, _______,
+    BL_TOGG,          BL_STEP, KC_MS_U, _______, _______,          _______, _______, _______, _______, _______, _______, _______, _______, RESET,            _______,
+    _______,          KC_MS_L, KC_MS_D, KC_MS_R, _______,          _______, _______, _______, _______, _______, _______, _______, _______, _______,
+    _______,          KC_BTN1, KC_BTN3, KC_BTN2, _______,          _______, _______, _______, KC_MUTE, KC_VOLD, KC_VOLU, _______, _______,          _______, MO(_FN),
+    _______,          _______, _______, _______, _______,          _______, _______, _______, _______,                                     _______, _______, _______
   ),
 
 };
